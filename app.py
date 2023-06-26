0e176d337a5a147028ac9cf9bc6c9d1c39be5606
udayabhanu0314@gmail.com <udayabhanu0314@gmail.com>
2023-06-26 12:40:02 +0530

    third commit

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+def main():
+    html_temp = """
+    <div style="background-color:lightblue;padding:16px">
+    <h2 style="color:black";text-align:center> Health Insurance Cost Prediction Using ML</h2>
+    </div>
+    
+    """
+    
+    st.markdown(html_temp,unsafe_allow_html=True)
+
+    model=joblib.load('model_joblib_test')
+
+    p1=st.slider('Enter Your age ',18,100)
+    s1=st.selectbox('Sex',('male','Female'))
+
+    if s1=='male':
+        p2=1
+    else:
+        p2=0
+    p3 = st.number_input("Enter Your BMI Value")
+    p4 = st.slider("Enter Number of Children",0,4)
+    s2 = st.selectbox("Smoker",("Yes","No"))
+    if s2=='Yes':
+        p5=1
+    else:
+        p5=0
+        
+        
+    p6 = st.slider("Enter Your Region",1,4)
+
+    if st.button('Predict'):
+        pred= model.predict([[p1,p2,p3,p4,p5,p6]])
+
+        st.balloons()
+        st.success('Your Insurance Cost is {}'.format(round(pred[0],2)))
+        
+        st.success('')
+
+if __name__ == '__main__':
+    main()
